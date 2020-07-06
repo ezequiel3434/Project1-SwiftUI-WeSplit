9650965bfc3851daff2786755bf61fbb6121b714
Ezequiel <ezequiel3434@gmail.com>
2020-07-06 14:25:35 -0300

    Initial Commit

---
+++ Project1-SwiftUI-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1-SwiftUI-WeSplit
+//
+//  Created by Ezequiel Parada Beltran on 06/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
